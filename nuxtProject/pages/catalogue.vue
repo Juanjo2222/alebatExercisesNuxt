c02dbb79186105aae66013799649ce43d9a45a5f
Juan Jose <juanjomillanguerra@gmail.com>
2025-04-01 16:27:40 +0200

    exercise 1 completed

---
+++ nuxtProject/pages/catalogue.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const bienvenida = "Bienvenido a catálogos";
+const buttonTxt = "Ir a home";
+</script>
+<template>
+  <h1>{{ bienvenida }}</h1>
+  <NuxtLink to="/">
+    <button class="to-home-button">{{ buttonTxt }}</button>
+  </NuxtLink>
+</template>
+<style>
+.to-home-button {
+  background-color: rgb(245, 226, 164);
+  border-radius: 2rem;
+}
+</style>
---
+++ nuxtProject/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const bienvenida = "Bienvenido a la home";
+const buttonTxt = "Ir a catálogos";
+</script>
+<template>
+  <h1>{{ bienvenida }}</h1>
+  <NuxtLink to="/catalogue">
+    <button class="to-catalogue-button">{{ buttonTxt }}</button>
+  </NuxtLink>
+</template>
+<style>
+.to-catalogue-button {
+  background-color: rgb(245, 226, 164);
+  border-radius: 2rem;
+}
+</style>
